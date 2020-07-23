8b4a883c4dd83e18298efa0dfaa8fd18bf3b5b75
Kannan Balasubramanian <rbkannan1@gmail.com>
2020-07-23 23:40:11 +0530

    Custom Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Routes from './Routing';
+import { LoginCallback, SecureRoute } from '@okta/okta-react';
+import { useOktaAuth } from '@okta/okta-react';
 import './App.css';
 
+const CALLBACK_PATH = '/implicit/callback';
+
 function App() {
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+
+  // useEffect(() => {
+  //   if (!authState.isAuthenticated) {
+  //     authService.login('/');
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    if (!authState.isAuthenticated) {
+      // When user isn't authenticated, forget any user info
+      setUserInfo(null);
+    } else {
+      authService.getUser().then((info) => {
+        setUserInfo(info);
+        console.log(authState)
+      });
+    }
+  }, [authState, authService]);
+
+  const login = () => {
+    authService.login('/');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Route path={CALLBACK_PATH} component={LoginCallback} />
+
+      {authState.isAuthenticated && !userInfo && (
+        <div>Loading user information...</div>
+      )}
+      {authState.isAuthenticated && userInfo && (
+        <>
+          <div>
+            <p>
+              Welcome back,
+              {userInfo.name}!
+            </p>{' '}
+          </div>
+          <Switch>
+            {Routes.map((route) => (
+              <SecureRoute
+                exact
+                path={route.path}
+                component={route.component}
+              ></SecureRoute>
+            ))}
+            <Redirect from='/' to='home' />
+          </Switch>
+        </>
+      )}
+      {!authState.isAuthenticated && (
+        <button id='login-button' primary onClick={login}>
+          Login
+        </button>
+      )}
     </div>
   );
 }
---
+++ src/Routing.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import Dashboard from './Dashboard';
+
+const Routes = [
+    {
+        path: '/home', component: Home
+    },
+    {
+        path: '/dashboard', component: Dashboard
+    }
+];
+
+export default Routes;
